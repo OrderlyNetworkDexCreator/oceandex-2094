e48c1dbc949e7a560da3249c8e693d339ca9daaa
Mario Reder <mario.reder@pm.me>
2025-09-28 01:33:43 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -23,7 +23,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TELEGRAM_URL": "",
   "VITE_DISCORD_URL": "",
   "VITE_TWITTER_URL": "",
-  "VITE_SEO_SITE_NAME": "OceanDex",
+  "VITE_SEO_SITE_NAME": "GrokDex",
   "VITE_SEO_SITE_DESCRIPTION": "",
   "VITE_SEO_SITE_URL": "https://dex.orderly.network/oceandex-2094/",
   "VITE_SEO_SITE_LANGUAGE": "en",
