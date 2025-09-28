5bed44036c62e85a7df7fa32c99972a1e55f1dba
Mario Reder <mario.reder@pm.me>
2025-09-28 21:32:24 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -3,9 +3,9 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_ORDERLY_BROKER_NAME": "GrokDex",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "false",
-  "VITE_ORDERLY_MAINNET_CHAINS": "56,1,900900900,42161,8453,10",
+  "VITE_ORDERLY_MAINNET_CHAINS": "56,900900900,42161,8453,10,1",
   "VITE_ORDERLY_TESTNET_CHAINS": "421614,84532,97,901901901,11124",
-  "VITE_DEFAULT_CHAIN": "",
+  "VITE_DEFAULT_CHAIN": "56",
   "VITE_PRIVY_APP_ID": "",
   "VITE_PRIVY_LOGIN_METHODS": "email",
   "VITE_PRIVY_TERMS_OF_USE": "",
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_SECONDARY_LOGO": "false",
   "VITE_ENABLED_MENUS": "Rewards,Markets,Trading,Portfolio,Leaderboard,Vaults",
   "VITE_CUSTOM_MENUS": "",
-  "VITE_ENABLE_CAMPAIGNS": "false",
+  "VITE_ENABLE_CAMPAIGNS": "true",
   "VITE_TELEGRAM_URL": "",
   "VITE_DISCORD_URL": "",
   "VITE_TWITTER_URL": "",
@@ -34,5 +34,5 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_AVAILABLE_LANGUAGES": "en,zh,ja,es,ko,vi,de,fr,ru,id,tr,it,pt,uk,pl,nl",
   "VITE_USE_CUSTOM_PNL_POSTERS": "false",
   "VITE_CUSTOM_PNL_POSTER_COUNT": "0",
-  "VITE_TRADING_VIEW_COLOR_CONFIG": ""
+  "VITE_TRADING_VIEW_COLOR_CONFIG": "{\"upColor\":\"#00C896\",\"downColor\":\"#F7525F\",\"pnlUpColor\":\"#00C896\",\"pnlDownColor\":\"#F7525F\",\"chartBG\":\"#131722\"}"
 };
